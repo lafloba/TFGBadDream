c7978ec5135ac5209598f149752f9316920511cd
al394511 <al394511@uji.es>
2024-05-21 12:40:37 +0200

    menu volumen y musica

---
+++ TFG/Assets/Scripts/MenuOpciones.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuOpciones : MonoBehaviour
+{
+    public AudioSource audioSource; // Referencia al AudioSource
+    public Toggle muteToggle; // Referencia al Toggle
+
+    void Start()
+    {
+        // Asegúrate de que el toggle esté en el estado correcto al iniciar el juego
+        bool isMuted = PlayerPrefs.GetInt("Muted", 0) == 1;
+        muteToggle.isOn = isMuted;
+        SetMute(isMuted);
+
+        // Suscribirse al evento de cambio del Toggle
+        muteToggle.onValueChanged.AddListener(delegate {
+            MuteUnmute(muteToggle.isOn);
+        });
+    }
+
+    public void MuteUnmute(bool isMuted)
+    {
+        SetMute(isMuted);
+        // Guardar el estado del mute en PlayerPrefs
+        PlayerPrefs.SetInt("Muted", isMuted ? 1 : 0);
+    }
+
+    private void SetMute(bool isMuted)
+    {
+        audioSource.mute = isMuted;
+    }
+}
+
